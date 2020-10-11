8231be3e90810c003353b7439e841fd931c4a4d6
JustinFirsching <JustinF52399@gmail.com>
2020-10-11 00:06:31 -0400

    Initial Project Base

---
+++ src/TextEncryption/__init__.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractclassmethod, abstractstaticmethod
+from typing import Any, Generic, TypeVar, Union, Type
+
+
+KeyTypeT = TypeVar('KeyTypeT', bound='Key')
+class Key(Generic[KeyTypeT]):
+    def __init__(self, key_data: KeyTypeT):
+        self._key_data = key_data
+        self._key_type = type(key_data)
+
+    def get_key(self) -> KeyTypeT:
+        return self._key_data
+
+    def get_key_type(self) -> Type:
+        return self._key_type
+
+
+class KeyModifier(ABC):
+    @abstractstaticmethod
+    def modify_key(key: Key, message: str) -> Key:
+        pass
+
+
+class Cipher(ABC):
+    @abstractstaticmethod
+    def encrypt_message(message: str, key: Key) -> str:
+        pass
+
+    @abstractstaticmethod
+    def decrypt_message(message: str, key: Key) -> str:
+        pass
+
+    @abstractstaticmethod
+    def generate_key(key_size: int) -> Key:
+        pass
+
